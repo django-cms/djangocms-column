16a1af5ad995a1f2a3d3df736795ae002ab6812f
Vinit Kumar <vinit.kumar@kidskonnect.nl>
2022-03-11 21:16:15 +0530

    fix: remove usage of ugettext_lazy from the code

--- djangocms_column/cms_plugins.py
+++ djangocms_column/cms_plugins.py
@@ -1,4 +1,4 @@
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 from cms.models import CMSPlugin
 from cms.plugin_base import CMSPluginBase
