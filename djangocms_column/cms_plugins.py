a5f59db0815dc7cf58a723e202c634ba9e856f51
Aleks Acimovic <alex@multi-task.hr>
2024-05-10 08:14:01 +0200

    Update cms_plugins.py - now using cms.api to add columns
    Using cms.api to make the plugin compatible with django-cms v4 as well. This patch also works for django-cms v3

--- djangocms_column/cms_plugins.py
+++ djangocms_column/cms_plugins.py
@@ -3,7 +3,7 @@ from django.utils.translation import gettext_lazy as _
 from cms.models import CMSPlugin
 from cms.plugin_base import CMSPluginBase
 from cms.plugin_pool import plugin_pool
-
+from cms import api
 from .forms import MultiColumnForm
 from .models import Column, MultiColumns
 
@@ -22,13 +22,12 @@ class MultiColumnPlugin(CMSPluginBase):
             request, obj, form, change
         )
         for _x in range(int(form.cleaned_data['create'])):
-            col = Column(
-                parent=obj,
+            col = api.add_plugin(
                 placeholder=obj.placeholder,
+                plugin_type=ColumnPlugin.__name__,
                 language=obj.language,
+                target=obj,
                 width=form.cleaned_data['create_width'],
-                position=CMSPlugin.objects.filter(parent=obj).count(),
-                plugin_type=ColumnPlugin.__name__
             )
             col.save()
         return response
